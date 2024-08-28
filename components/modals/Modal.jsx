20fc807ef8bdfe1e6337e25e1ce58b196d72107e
francisco <francisco.leyescampoy@gmail.com>
2024-08-28 20:17:02 -0300

    se mejoro el modal

--- components/modals/Modal.jsx
+++ components/modals/Modal.jsx
@@ -1,32 +1,41 @@
 'use client';
-import YouTube from 'react-youtube'; // Importar el componente de react-youtube
+import YouTube from 'react-youtube';
 
 const Modal = ({ isOpen, setIsOpen, movieData, children }) => {
   const handleClose = (e) => {
     e.preventDefault();
     setIsOpen(false);
   };
-
   if (!isOpen) return null;
- 
   const videoOptions = {
-    height: '640',
-    width: '860',
     playerVars: {
-      autoplay: 1, 
+      autoplay: 1,
     },
   };
+
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center container">
-      {/*Background overlay */}
+    <div className="fixed inset-0 z-50 flex items-center justify-center">
+      {/* Background overlay */}
       <div
         className="fixed inset-0 bg-black opacity-50"
         onClick={handleClose}></div>
 
       {/* Modal content */}
-      <div className=" p-4 rounded-lg shadow-lg  ">
+      <div className="relative p-4 rounded-lg shadow-lg w-[90%] max-w-3xl mx-auto">
         {children}
-        <YouTube videoId={movieData[0]?.key} opts={videoOptions} />
+        <div className="w-full h-0 pb-[56.25%] relative">
+          {' '}
+          {/* 16:9 Aspect Ratio */}
+          <YouTube
+            videoId={movieData[0]?.key}
+            opts={{
+              ...videoOptions,
+              width: '100%',
+              height: '100%',
+            }}
+            className="absolute inset-0 w-full h-full"
+          />
+        </div>
       </div>
     </div>
   );
